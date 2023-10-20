224f8c1c8c69423c60c592ad3831dc5ae1c143e8
dffxd-suntra <qawseswa1931@gmail.com>
2023-10-20 16:24:39 +0800

    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import "./lit-booru";
 
+// keep cors proxy on
+// 4.5min per fetch
+setInterval(() => fetch("https://tame-local-branch.glitch.me/"), 4.5 * 60 * 1000);
+
 declare global {
     interface Window {
         clarity: any;
