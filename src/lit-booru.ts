36818fe440ab41c86cdeee2abb0880465d3aa3c6
dffxd-suntra <qawseswa1931@gmail.com>
2023-10-27 21:27:48 +0800

    modified:   src/lit-booru.ts

--- src/lit-booru.ts
+++ src/lit-booru.ts
@@ -8,31 +8,6 @@ import "./component/booru-search";
 import "./component/booru-thumbnail";
 import "./component/booru-viewer";
 
-interface picInfo {
-    id: number,
-    tags: string,
-    height: number,
-    width: number,
-    file_url: string,
-    has_notes: boolean
-    hash: string,
-    image: string,
-    owner: string
-    parent_id: number,
-    preview_url: string,
-    rating: string,
-    sample: boolean,
-    sample_height: number,
-    sample_url: string,
-    sample_width: number,
-    score: number,
-    source: string,
-    change: number,
-    comment_count: number,
-    directory: number,
-    status: string
-};
-
 @customElement("lit-booru")
 export class LitBooru extends LitElement {
     @property({ type: Number })
